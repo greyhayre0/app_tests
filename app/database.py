1956c63da4256ab9f0b2764b94fd8cbf7bfdeb77
Grey-Hayred <greyhayre1@gmail.com>
2025-09-03 02:24:41 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://app_user:app_password@db:5432/app_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
